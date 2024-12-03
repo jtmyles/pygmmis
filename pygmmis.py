ab820ff534d54168c1ff1f91b4f498f2f5a3a3c9
Justin Myles <justin.t.myles@gmail.com>
2024-12-02 19:41:57 -0500

    rm use of im_func, im_self, & im_class

--- pygmmis.py
+++ pygmmis.py
@@ -32,9 +32,9 @@ def createShared(a, dtype=ctypes.c_double):
 # this is to allow multiprocessing pools to operate on class methods:
 # https://gist.github.com/bnyeggen/1086393
 def _pickle_method(method):
-	func_name = method.im_func.__name__
-	obj = method.im_self
-	cls = method.im_class
+	func_name = method.__func__.__name__ # func_name = method.im_func.__name__
+	obj = method.__self__ # obj = method.im_self
+	cls = method.__self__.__class__# cls = method.im_class
 	if func_name.startswith('__') and not func_name.endswith('__'): #deal with mangled names
 		cls_name = cls.__name__.lstrip('_')
 		func_name = '_' + cls_name + func_name
